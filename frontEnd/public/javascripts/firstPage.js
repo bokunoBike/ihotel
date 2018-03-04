7c87948d5c552ad9ceba57df2dc91777c186bb4a
wanli <1074562414@qq.com>
2018-03-04 16:53:12 +0800

    loginOverArea

--- frontEnd/public/javascripts/firstPage.js
+++ frontEnd/public/javascripts/firstPage.js
@@ -20,6 +20,7 @@ $(document).ready(function()
             {
                 roomNumber:
                 {
+                    threshold: 4,
                     message: 'The username is not valid',
                     validators:
                     {
@@ -30,28 +31,35 @@ $(document).ready(function()
                         regexp:
                         {/* 正则表达式*/
                            regexp: /^[0-9]*[1-9][0-9]*$/,
-                           message: '请检查房号正确性'
+                           message: '房号不存在'
                        },
                         stringLength:
                         {/*长度提示*/
                             min: 3,
                             max: 4,
-                            message: '请检查房号正确性'
+                            message: '房号不存在'
                         },
-                        threshold: 3,
                         remote:
                         {
-                          url:'后边改！！！',
+                          url:'localhost:8000/login/login',
                           message: '房号不存在',
                           delay: 1000,
                           type:'POST',
-                          data: {"roomNumber": document.getElementById('roomNumber').value}
+                          data: {"roomNumber": document.getElementById('roomNumber').value},
+                          dataType: 'json',
+                          // 下面两个参数解决跨域问题
+                          xhrFields: {
+                             withCredentials: true
+                        },
+                          crossDomain: true,
+                          complete: function(XMLHttpRequest, textStatus) {},
                         }
                     }
                 },
                 thePassword:
                 {
                     //trigger: 'blur',
+                    threshold: 6,
                     message:'密码无效',
                     validators:
                     {
@@ -59,7 +67,6 @@ $(document).ready(function()
                         {
                             message: '密码不能为空'
                         },
-                        threshold: 6,
                         regexp:
                         {/* 正则表达式*/
                            regexp: /(^\d{5}(\d|X|x)$)/,
@@ -89,7 +96,36 @@ $(document).ready(function()
           e.preventDefault();//避免重复提交
           var $form = $(e.target);
           var bv = $form.data('bootstrapValidator');
-          $.post($form.attr('action'),$form.serialize(),function(result){
+          $.ajax({
+            type: 'POST',
+            url:'localhost:8000/login/login',
+            dataType: 'json',
+            data: {"roomNumber": document.getElementById('roomNumber').value,
+                   "thePassword": document.getElementById('thePassword').value},
+            // 下面两个参数解决跨域问题
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            complete: function(XMLHttpRequest, textStatus) {},
+            success: function(data) {
+              if(data.valid==true)
+              {
+                window.location.href='http://localhost:3000/userPage';
+              }
+              else
+              {
+                bootstrapValidator.updateStatus('thePassword', 'INVALID').validateField('username');
+                //$('#defaultForm').data(“bootstrapValidator”).updateStatus("thePassword",  "NOT_VALIDATED",  null );
+                bootstrapValidator.updateMessage('thePassword',null,'密码错误');
+              }
+            },
+            error: function(err) {
+                console.log(err);
+            }
+          });
+
+          /*$.post($form.attr('action'),$form.serialize(),function(result){
             if(result.valid==true)
             {
               window.location.href='http://localhost:3000/userPage';
@@ -100,6 +136,6 @@ $(document).ready(function()
               //$('#defaultForm').data(“bootstrapValidator”).updateStatus("thePassword",  "NOT_VALIDATED",  null );
               bootstrapValidator.updateMessage('thePassword',null,'密码错误');
             }
-          },'json');
+          },'json');*/
         });
 });
