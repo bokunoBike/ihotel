a3b2cd560caa8b6e446d676a3804ad1e9a560952
wanli <1074562414@qq.com>
2018-03-13 21:22:55 +0800

    checkinAgain

--- frontEnd/public/javascripts/userPage.js
+++ frontEnd/public/javascripts/userPage.js
@@ -1,3 +1,25 @@
+const text = document.querySelector('.text')
+const svg = document.querySelector('svg')
+
+const support = 'd' in text.style
+
+
+if (!support) {
+
+// 	bring text in center
+	text.classList.add('center')
+
+// 	blur svg element
+	svg.classList.add('blur')
+}
+
+var numOfPeople =1;
+
+if(numOfPeople == 1)
+	{
+		svg.setAttribute('d','M0,0 L50,0 L50,50 L50,100 L0,100 L0,25 L0,0')
+	}
+
 $('#myModal').on('show.bs.modal', function () {
 	document.getElementsByTagName('body')[0].style.paddingTop='0';
 	document.getElementById('navbarContainer').style.position='static';
