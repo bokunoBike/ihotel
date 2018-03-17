5e0083be37a34e8d622bc93779b5e9227975b522
zzqz <sdsbshe@163.com>
2018-03-17 12:46:39 +0800

    add websocket

--- backEnd/ihotel/wsgi.py
+++ backEnd/ihotel/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from os.path import join, dirname, abspath
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+import sys
+sys.path.insert(0, PROJECT_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ihotel.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "ihotel.settings"
 
 application = get_wsgi_application()
+
