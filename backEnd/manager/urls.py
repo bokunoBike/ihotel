08ddae8c8b0ec1278d2f6bc6c028ccbc85294234
zzqz <sdsbshe@163.com>
2018-03-30 19:55:48 +0800

    330-2

--- backEnd/manager/urls.py
+++ backEnd/manager/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 app_name = 'manager'
 urlpatterns = [
+    url(r'^set_room_people_counts', set_room_people_counts, name="set_room_people_counts"),
+    url(r'^get_floor_rooms', get_floor_rooms, name="get_floor_rooms"),
 ]
