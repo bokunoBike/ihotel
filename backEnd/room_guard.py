1c86deeb07fe75dbf13f651b561b0cb92754d492
zzqz <sdsbshe@163.com>
2018-03-31 17:30:25 +0800

    331-2

---
+++ backEnd/room_guard.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import pymysql
+import time
+import datetime
+
+# 连接mysql
+db = pymysql.connect("localhost", "root", "", "ihotel")
+cursor = db.cursor()
+
+while True:
+    current_time = datetime.datetime.now()
+    sql = "UPDATE Room SET pattern='0' WHERE expired_time < '%s %s:%s:%s';" % (
+        current_time.date(), current_time.hour, current_time.minute, current_time.second)
+    i = cursor.execute(sql)
+    db.commit()
+    time.sleep(10)
+cursor.close()
+db.close()
